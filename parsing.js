fa4251039874fcd6c09d34cc8ddd0b462706af82
ace9809 <tktmdwns@naver.com>
2017-02-23 15:55:39 +0900

    stat으로 파일 유무 확인후 파일이 없으면 json 파일생성 있으면 파일 데이터들 비교

--- parsing.js
+++ parsing.js
@@ -2,34 +2,32 @@ var request = require("request");
 var cheerio = require("cheerio");
 var fs = require("fs");
 var request_sync = require('sync-request');
-var url = "http://comic.naver.com/webtoon/weekday.nhn";
-var noId,urlSplit,webtoonJson,urlSplit2,detailLInk;
-var webtoonCollection = [];
-// var testJson = require('./test.json');
 
-request(url, function(error, response, body) {
+request("http://comic.naver.com/webtoon/weekday.nhn", function(error, response, body) {
   // console.log(JSON.stringify(testJson));
   // console.log(testJson[1].no);
   // testJson.name = "test";
   // console.log(JSON.stringify(testJson));
-
+  var webtoonCollection = [];
   if (error) throw error;
   var $ = cheerio.load(body);
   $("#content").find(".title").each(function (i) {
     webtoon_list = $(this);
-    var link = 'http://comic.naver.com' + webtoon_list.attr('href');
-    var title = webtoon_list.attr("title");
+    var link = 'http://comic.naver.com' + webtoon_list.attr('href'); //웹툰 메인홈에서 가져온 title값이 들어
+    var title = webtoon_list.attr("title"); //웹툰 메인홈에서 제목 가져옴
+    console.log(title);
     var res = request_sync('GET', link, {
       'headers': {
         'user-agent': 'example-user-agent'
-      }});
+      }}); //request_sync로  link가 each문을 나가면 마지막 값만 출력되는 걸 막아줌
     var episodes = cheerio.load(res.getBody());
     episodes(".title").find("a").first().each(function (i) {
       noId = episodes(this).attr("href");
       urlSplit = noId.split("&");
-      urlSplit2 = urlSplit[1].split("=");
+      urlSplit2 = urlSplit[1].split("="); //urlSplit2에 들어오는 값: noid값
       // detailLink = 'http://comic.naver.com' + noId;
     })
+
     // request(detailLink, function(error, response, body) {
     //   console.log(response.statusCode);
     // }) 디테일 링크 응답코드
@@ -38,25 +36,34 @@ request(url, function(error, response, body) {
     webtoonObj.title = title;
     webtoonObj.no = urlSplit2[1];
     webtoonCollection.push(webtoonObj);
-    var jsonInfo = JSON.stringify(webtoonCollection);
-    console.log(jsonInfo);
-    // if (!testJson) {
-    //   fs.writeFileSync('test.json', jsonInfo, 'utf8');
-    // }
-    // else {
-    //   if(urlSplit2[1] !== testJson[i].no)
-    //   {
-    //     console.log(title);
-    //     console.log("전에id:" + testJson[i].no + "새로가져온 id :"+urlSplit2[1]);
-    //     console.log("출력");
-    //     fs.writeFileSync('test.json', jsonInfo, 'utf8');
-    //   }
-    //   else if(urlSplit2[1] === testJson[i].no) {
-    //     console.log("같음");
-    //     console.log(title);
-    //     console.log("전에id:" + testJson[i].no + "새로가져온 id :"+urlSplit2[1]);
-    //   }
-    // }
+    jsonInfo = JSON.stringify(webtoonCollection); //객체만든것들을 배열에다 집어넣은뒤 json형식으로 변환
+    // var fileState = fs.statSync('./test.json');
+    // var testJson = require("./test.json");
+    // console.log(testJson[i].no);
+    fs.stat('./test.json', function (err) {
+      if (err) {
+        fs.writeFileSync('test.json', jsonInfo, 'utf8', 'w');
+      }
+      else {
+        var testJson = require("./test.json");
+        if(webtoonCollection[i].no !== testJson[i].no)
+        {
+
+          console.log(title);
+          console.log(webtoonCollection[i].no);
+          console.log("전에id:" + testJson[i].no + "새로가져온 id :"+ webtoonCollection[i].no);
+          console.log("출력");
+          fs.writeFileSync('test.json', jsonInfo, 'utf8');
+        }
+        else {
+          console.log(webtoonCollection[i].no);
+          console.log("같음");
+          console.log(title);
+          console.log("전에id:" + testJson[i].no + "새로가져온 id :"+ webtoonCollection[i].no);
+        }
+      }
+    }) //파일상태를 확인한후 현재 디렉토리에 test.json파일이 없으면 세션이 끊어지는 에러가 생기는데 그 에러가 생겼을 시 test.json을 만든다 만약 파일이 확인되서 에러가 나지 않았을 경우에 test.json을 불러온뒤 webtoonCollection에 있는 no값과 json파일에 저장되어 있는 no값을 비교해 다르면 jsonInfo파일을 저장 같으면 그냥 무시
+
 
     // console.log(webtoonJson);
     // episodes(".detail").find(".wrt_nm").e이ach(function (i) {
@@ -65,3 +72,4 @@ request(url, function(error, response, body) {
 
   });
 });
+
